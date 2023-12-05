f572d6a25cfbf1976752f0513718a672ee035a5b
Amdadul Haq <amdadulhaq781@gmail.com>
2023-12-06 01:45:27 +0600

    Update UserCreatorCommand.php

--- src/Commands/UserCreatorCommand.php
+++ src/Commands/UserCreatorCommand.php
@@ -20,7 +20,7 @@ class UserCreatorCommand extends Command implements PromptsForMissingInput
         $password = $this->argument('password');
 
         // Check if user with the provided email already exists
-        $existingUser = User::where('email', $email)->first();
+        $existingUser = User::where('email', $email)->first(); /*@phpstan-ignore-line */
         if ($existingUser) {
             $this->error('User with this email already exists.');
             $this->newLine();
@@ -29,13 +29,13 @@ class UserCreatorCommand extends Command implements PromptsForMissingInput
         }
 
         // Create a new user
-        $user = new User();
-        $user->name = $name;
-        $user->email = $email;
-        $user->password = Hash::make($password);
+        $user = new User(); /*@phpstan-ignore-line */
+        $user->name = $name; /*@phpstan-ignore-line */
+        $user->email = $email; /*@phpstan-ignore-line */
+        $user->password = Hash::make($password); /*@phpstan-ignore-line */
         // Add any additional fields as needed
 
-        $user->save();
+        $user->save(); /*@phpstan-ignore-line */
 
         $this->info('User created successfully.');
         $this->newLine();
