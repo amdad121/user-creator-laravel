5c179573b50250f2f0d208b7896fbbea83102bc3
Amdadul Haq <amdadulhaq781@gmail.com>
2023-12-10 17:48:03 +0600

    strict type added

--- src/UserCreatorServiceProvider.php
+++ src/UserCreatorServiceProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace AmdadulHaq\UserCreator;
 
 use AmdadulHaq\UserCreator\Commands\UserCreatorCommand;
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use AmdadulHaq\UserCreator\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
